0d17b5eb2daa7b24e217b90476eaea8df7c7edba
Sruti Guhathakurta <sruti@gatech.edu>
2016-11-05 01:51:04 -0400

    login is basic af

--- app/src/main/java/com/aroundme/around/LoginActivity.java
+++ app/src/main/java/com/aroundme/around/LoginActivity.java
@@ -2,6 +2,7 @@ package com.aroundme.around;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void handleLoginClicked(View view) {
     }
 }
