9c5c46dcfe116428d063a19f0b7ed004f357183c
patrickcaruso <patrickec246@gmail.com>
2016-11-05 21:45:52 -0400

    jkughjtd

--- app/src/main/java/com/aroundme/around/controllers/Holder.java
+++ app/src/main/java/com/aroundme/around/controllers/Holder.java
@@ -10,7 +10,7 @@ public class Holder {
 
     public static Bitmap holder;
     public static LocationTracker tracker;
-    public static int distance = 100;
+    public static int distance = 999999;
 
     public static String user;
     public static int id;
